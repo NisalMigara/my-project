ea69c9ebe1ba01d3961135180c8a9ea39ff9d527
dasunig <dasgeeth20@gmail.com>
2023-10-16 18:51:07 +0530

    Added a another python program to third branch

--- 206140E_Nisal.py
+++ 206140E_Nisal.py
@@ -11,6 +11,20 @@ random_number = generate_random_number()
 print(random_number)
 
 
+def is_prime(n):
+  if n <= 1:
+    return False
+  for i in range(2, n):
+    if n % i == 0:
+      return False
+  return True
+
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+  print(num, "is a prime number.")
+else:
+  print(num, "is not a prime number.")
 
 
 
